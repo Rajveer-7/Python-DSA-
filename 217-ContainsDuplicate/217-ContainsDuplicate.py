05e11518b05fce0de2b43d9eda8602cb5e0c52e1
Rajveer Singh <118693440+Rajveer-7@users.noreply.github.com>
2025-11-24 08:35:56 -0500

    File updated at 11/24/2025, 8:35:55 AM

--- 217-ContainsDuplicate/217-ContainsDuplicate.py
+++ 217-ContainsDuplicate/217-ContainsDuplicate.py
@@ -1,10 +1,19 @@
-# Last updated: 11/21/2025, 3:26:10 AM
-class Solution(object):
-    def containsDuplicate(self, nums):
-        hashset = set()
+# Last updated: 11/24/2025, 8:35:55 AM
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        countS, countT = {}, {}
+
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+
+        for c in countS:
+            if countS[c] != countT.get(c, 0):
+                return False
         
-        for n in nums:
-            if n in hashset:
-                return True
-            hashset.add(n)
-        return False
+        return True
+
+
