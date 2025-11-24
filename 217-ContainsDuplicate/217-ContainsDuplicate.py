044e7772ee2b511247b1b22ff053e4eaca845aca
Rajveer Singh <118693440+Rajveer-7@users.noreply.github.com>
2025-11-24 11:20:44 -0500

    File updated at 11/24/2025, 11:20:43 AM

--- 217-ContainsDuplicate/217-ContainsDuplicate.py
+++ 217-ContainsDuplicate/217-ContainsDuplicate.py
@@ -1,14 +1,22 @@
-# Last updated: 11/24/2025, 11:06:16 AM
+# Last updated: 11/24/2025, 11:20:43 AM
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        hmap = {}
-        ans =[]
-        for n in nums1:
-            hmap[n] = hmap.get(n,0) + 1
-        
-        for n in nums2:
-            if n in hmap and hmap[n] > 0:
-                hmap[n]-=1
-                ans.append(n)
+        ## two pointer solution 
+        nums1, nums2 = sorted(nums1), sorted(nums2)
+        p1 = p2 = 0
+        n, m = len(nums1), len(nums2)
+        ans = []
+
+        while p1 < n and p2<m:
+            if nums1[p1] == nums2[p2]:
+                ans.append(nums1[p1])
+                p1+=1
+                p2+=1
+                continue
+            
+            if nums1[p1] > nums2[p2]:
+                p2 += 1
+            else:
+                p1 += 1
 
         return ans
