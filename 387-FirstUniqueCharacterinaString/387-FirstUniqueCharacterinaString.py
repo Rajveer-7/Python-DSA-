1ee1e4f425679a1488e033171d8f52015c13aeb0
Rajveer Singh <118693440+Rajveer-7@users.noreply.github.com>
2025-12-12 12:13:29 -0500

    File updated at 12/12/2025, 12:13:29 PM

--- 387-FirstUniqueCharacterinaString/387-FirstUniqueCharacterinaString.py
+++ 387-FirstUniqueCharacterinaString/387-FirstUniqueCharacterinaString.py
@@ -1,16 +1,17 @@
-# Last updated: 12/12/2025, 11:43:22 AM
+# Last updated: 12/12/2025, 12:13:29 PM
 1class Solution:
-2    def firstUniqChar(self, s: str) -> int:
-3        # Map the character to its count 
-4        #L-> 1, e->3, t->1 , - Hashmap , iterate over string  O(n) , then we return the index of the character 
-5        #We make a Hashmap ,
-6
-7        count = defaultdict(int) # char -> count
+2    def isIsomorphic(self, s: str, t: str) -> bool:
+3        #Hashmap - O(n) - iterating through both strings, 
+4        mapST, mapTS = {}, {}
+5
+6        for i in range(len(s)):  # for c1, c2 in zip(s,t)
+7            c1, c2 = s[i], t[i]
 8
-9        for c in s:
-10            count[c] += 1
-11
-12        for i,c in enumerate(s):
-13            if count[c] ==1:
-14                return i
-15        return -1
+9            if ((c1 in mapST and mapST[c1] != c2) or
+10                (c2 in mapTS and mapTS[c2] != c1)):
+11                return False
+12
+13
+14            mapST[c1] = c2
+15            mapTS[c2] = c1
+16        return True
