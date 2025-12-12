881b92107fd6c7c87e60733902fbf3ac065ff8de
Rajveer Singh <118693440+Rajveer-7@users.noreply.github.com>
2025-12-12 13:24:21 -0500

    File updated at 12/12/2025, 1:24:19 PM

--- 387-FirstUniqueCharacterinaString/387-FirstUniqueCharacterinaString.py
+++ 387-FirstUniqueCharacterinaString/387-FirstUniqueCharacterinaString.py
@@ -1,23 +1,30 @@
-# Last updated: 12/12/2025, 1:23:07 PM
+# Last updated: 12/12/2025, 1:24:19 PM
 1class Solution:
 2    def wordPattern(self, pattern: str, s: str) -> bool:
-3        # if words or chars are more , we return false 
-4
-5        # we make 2 hashmaps 
-6        words = s.split(" ")
+3        # Split s into words
+4        words = s.split(" ")
+5
+6        # If lengths differ, we can't have a 1-to-1 mapping
 7        if len(pattern) != len(words):
 8            return False
 9
-10        charToWord = {}
-11        wordToChar = {}
-12
-13        for c, w in zip(pattern, words):
-14            if c in charToWord and charToWord[c] != w :
-15                return False
-16            if w in wordToChar and wordToChar[w] != c:
-17                return False 
-18
-19            charToWord[c] = w
-20            wordToChar[w] = c
-21        
-22        return True
+10        # charToWord: pattern char -> word
+11        # wordToChar: word -> pattern char
+12        charToWord = {}
+13        wordToChar = {}
+14
+15        for c, w in zip(pattern, words):
+16            # If we've seen c before, it must map to the same word w
+17            if c in charToWord and charToWord[c] != w:
+18                return False
+19
+20            # If we've seen w before, it must map to the same char c
+21            if w in wordToChar and wordToChar[w] != c:
+22                return False
+23
+24            # Record the mapping in both directions
+25            charToWord[c] = w
+26            wordToChar[w] = c
+27
+28        return True
+29
