65c1af34348914c4b531403ba5fa178ed2e96792
Rajveer Singh <118693440+Rajveer-7@users.noreply.github.com>
2025-12-12 13:23:08 -0500

    File updated at 12/12/2025, 1:23:07 PM

--- 387-FirstUniqueCharacterinaString/387-FirstUniqueCharacterinaString.py
+++ 387-FirstUniqueCharacterinaString/387-FirstUniqueCharacterinaString.py
@@ -1,17 +1,23 @@
-# Last updated: 12/12/2025, 12:13:29 PM
+# Last updated: 12/12/2025, 1:23:07 PM
 1class Solution:
-2    def isIsomorphic(self, s: str, t: str) -> bool:
-3        #Hashmap - O(n) - iterating through both strings, 
-4        mapST, mapTS = {}, {}
-5
-6        for i in range(len(s)):  # for c1, c2 in zip(s,t)
-7            c1, c2 = s[i], t[i]
-8
-9            if ((c1 in mapST and mapST[c1] != c2) or
-10                (c2 in mapTS and mapTS[c2] != c1)):
-11                return False
+2    def wordPattern(self, pattern: str, s: str) -> bool:
+3        # if words or chars are more , we return false 
+4
+5        # we make 2 hashmaps 
+6        words = s.split(" ")
+7        if len(pattern) != len(words):
+8            return False
+9
+10        charToWord = {}
+11        wordToChar = {}
 12
-13
-14            mapST[c1] = c2
-15            mapTS[c2] = c1
-16        return True
+13        for c, w in zip(pattern, words):
+14            if c in charToWord and charToWord[c] != w :
+15                return False
+16            if w in wordToChar and wordToChar[w] != c:
+17                return False 
+18
+19            charToWord[c] = w
+20            wordToChar[w] = c
+21        
+22        return True
